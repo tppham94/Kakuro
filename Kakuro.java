75d8c08fd71e7de75067e85223083c862409c9be
ivanivachenko <46464724+ivanivachenko@users.noreply.github.com>
2020-01-28 00:27:00 -0500

    Update Kakuro.java
    added validaton with colored indicators on the button when right solution is inputted

--- Kakuro.java
+++ Kakuro.java
@@ -1,7 +1,11 @@
+
 import java.awt.Color;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.BoxLayout;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -10,54 +14,54 @@ import javax.swing.JTextField;
 
 public class Kakuro {
 	public static void main(String[] args) {
-		JFrame jf = new JFrame();
+		JFrame jf = new JFrame("Kakuro");
 		// JPanel jp=new JPanel(new GridLayout());
 		JTextField t7, t8, t9, t10, t12, t13, t14, t15, t16, t17, t19, t20, t21, t23, t24, t25;
 		JLabel l1, l2, l3, l4, l5,l6, l11_a, l11_b, l18, l22;
 		JPanel jp = new JPanel();
 //adding labels in order
 		l1 = new JLabel("");
-		l2 = new JLabel("13");
-		l3 = new JLabel("11");
-		l4 = new JLabel("26");
-		l5 = new JLabel("22");
-		l6 = new JLabel("25");
+		l2 = new JLabel("13", JLabel.CENTER);
+		l3 = new JLabel("11", JLabel.CENTER);
+		l4 = new JLabel("26", JLabel.CENTER);
+		l5 = new JLabel("22", JLabel.CENTER);
+		l6 = new JLabel("25", JLabel.RIGHT);
 		// text field in order
-		t7 = new JTextField("4");
-		t8 = new JTextField("0");
-		t9 = new JTextField("8");
-		t10 = new JTextField("0");
+		t7 = new JTextField("");
+		t8 = new JTextField("");
+		t9 = new JTextField("");
+		t10 = new JTextField("");
 		//adding panel
 		l11_a = new JLabel("21");
 		l11_b = new JLabel("10");
 		jp.add(l11_a);
 		jp.add(l11_b);
 		//adding text field 
-		t12 = new JTextField("0");
-		t13 = new JTextField("0");
-		t14 = new JTextField("0");
-		t15 = new JTextField("0");
-		t16 = new JTextField("0");
-		t17 = new JTextField("0");
+		t12 = new JTextField("");
+		t13 = new JTextField("");
+		t14 = new JTextField("");
+		t15 = new JTextField("");
+		t16 = new JTextField("");
+		t17 = new JTextField("");
 		//Labels
-		l18 = new JLabel("11");
+		l18 = new JLabel("11", JLabel.RIGHT);
 	//text fields
 		
-		t19 = new JTextField("0");
-		t20 = new JTextField("0");
-		t21 = new JTextField("0");
+		t19 = new JTextField("");
+		t20 = new JTextField("");
+		t21 = new JTextField("");
 		//labels
-		l22 = new JLabel("15");
+		l22 = new JLabel("15", JLabel.RIGHT);
 		//textfields
-		t23 = new JTextField("0");
-		t24 = new JTextField("0");
-		t25 = new JTextField("0");
+		t23 = new JTextField("");
+		t24 = new JTextField("");
+		t25 = new JTextField("");
 		// Label--------
 	
 	
 		//jp.add(l11_a, l11_b);
 		
-//
+
 		jf.add(l1);
 	//	l1.setBackground(Color.black);
 		jf.add(l2);
@@ -66,7 +70,7 @@ public class Kakuro {
 		jf.add(l5);
 		jf.add(l6);
 	
-//Adding text feild
+//Adding text field
 		jf.add(t7);
 		jf.add(t8);
 		jf.add(t9);
@@ -90,9 +94,37 @@ l1.setBackground(Color.BLACK);
 
 		jp.setBackground(Color.gray);
 	
-
-		jf.setSize(400, 400);
-		jf.setLayout(new GridLayout(5, 5));
+		jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		jf.setSize(500, 500);
+		jf.setLayout(new GridLayout(6, 6));
 		jf.setVisible(true);
+		
+		JButton button = new JButton("validate");
+		jf.add(button);		
+		button.addActionListener(new ActionListener(){
+			 
+
+			@Override   
+			public void actionPerformed(ActionEvent arg0) {   //validates if all textfields are equal to the solution
+				if(t7.getText().equals("4") && t8.getText().equals("6") && t9.getText().equals("8") && t10.getText().equals("7") && t12.getText().equals("3") &&
+						t13.getText().equals("5") && t14.getText().equals("7") && t15.getText().equals("6") && t16.getText().equals("8") && t17.getText().equals("6") &&
+						t19.getText().equals("6") && t20.getText().equals("5") && t21.getText().equals("2") && t23.getText().equals("6") && t24.getText().equals("5") && 
+						t25.getText().equals("4")) {
+					System.out.println("true");
+					button.setBackground(Color.GREEN);
+				}else {
+					System.out.println("false");
+					button.setBackground(Color.RED);
+				}
+				
+				if(t7.getText().equals("4")) {
+					System.out.println("true2");
+					button.setBackground(Color.GREEN);
+				}
+			}
+			});
+		
+		
+		
 	}
 }
